10e5753aff5034fa46bca3a27f3b23da1d6cce59
MichaelSal <salariosamichael@gmail.com>
2023-04-18 21:06:25 -0700

    changed revision id

--- migrations/versions/ffdc0a98111c.py
+++ migrations/versions/ffdc0a98111c.py
@@ -14,7 +14,7 @@ SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
-revision = '2a740f34157f'
+revision = 'ffdc0a98111c'
 down_revision = None
 branch_labels = None
 depends_on = None
