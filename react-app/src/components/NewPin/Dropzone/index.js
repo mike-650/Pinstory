50579e6c1974872d07fe6ff883e4957fa61c2de7
MichaelSal <salariosamichael@gmail.com>
2023-04-26 18:33:11 -0700

    put validations for description on upload

--- react-app/src/components/NewPin/Dropzone/index.js
+++ react-app/src/components/NewPin/Dropzone/index.js
@@ -19,27 +19,11 @@ function Dropzone({ className, setImgFile, errors, uploadedFile, setUploadedFile
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
-    <>
-      {/* {errors.imgFile && <span className="NP-errors">{errors.imgFile}</span>} */}
+    <div className="testinglol">
       <div {...getRootProps({
         className: className
       }
       )}>
-
-        {/* <input {...getInputProps()} />
-        {
-          isDragActive ?
-            <div className="DZ-inner-text">
-              <i class="fa-solid fa-circle-arrow-up fa-rotate-180 fa-xl"></i>
-              <p>Drop the file here!</p>
-            </div>
-            :
-            <div className="DZ-inner-text">
-              <i class="fa-solid fa-circle-arrow-up fa-xl"></i>
-              <p>Drag and drop or click to upload</p>
-            </div>
-        } */}
-
         {uploadedFile ? (
           // The file has been uploaded, so render only the input
           <input {...getInputProps()} />
@@ -53,20 +37,22 @@ function Dropzone({ className, setImgFile, errors, uploadedFile, setUploadedFile
                 <p>Drop the file here!</p>
               </div>
             ) : (
-              <div className="DZ-inner-text">
-                <i className="fa-solid fa-circle-arrow-up fa-xl"></i>
-                <p>Drag and drop or click to upload</p>
+              <>
+                <div className="DZ-inner-text">
+                  <i className="fa-solid fa-circle-arrow-up fa-xl"></i>
+                  <p>Drag and drop or click to upload</p>
+                </div>
+                </>
+              )}
               </div>
-            )}
-          </div>
-        )}
+          )}
 
-        {uploadedFile &&
-          <img src={uploadedFile[0].preview} alt='preview' className="NP-image-preview" />
-        }
-      </div>
-    </>
-  )
+            {uploadedFile &&
+              <img src={uploadedFile[0].preview} alt='preview' className="NP-image-preview" />
+            }
+          </div>
+    </div>
+      )
 }
 
-export default Dropzone
+      export default Dropzone
--- react-app/src/components/NewPin/index.js
+++ react-app/src/components/NewPin/index.js
@@ -47,6 +47,7 @@ function NewPin() {
       ".raw", ".webp"]
 
     if (title.length >= 30) err.title = 'Pin Title must be less than 30 characters';
+    if (description.length >= 120) err.description = 'Description must be less than 120 characters';
 
     if (!imgFile) {
       err.imgFile = 'Please provide an image file'
@@ -100,8 +101,8 @@ function NewPin() {
             />
           </div>
           <div className='NP-create-pin-right-side'>
-            {errors.imgFile && <span className="NP-errors">{errors.imgFile}</span>}
-            {errors.title ? <p className='NP-errors'>{errors.title}</p> : null}
+            {errors.imgFile && <span className="NP-errors">* {errors.imgFile}</span>}
+            {errors.title ? <p className='NP-errors'>*  {errors.title}</p> : null}
             <input
               type='text'
               placeholder='Add your title'
@@ -120,6 +121,7 @@ function NewPin() {
                 <p>{user.lastName}</p>
               </div>
             </div>
+            {errors.description ? <p className='NP-errors' style={{marginTop:'30px'}}>* {errors.description}</p> : null}
             <textarea
               type='text'
               placeholder='Tell everyone what your Pin is about'
